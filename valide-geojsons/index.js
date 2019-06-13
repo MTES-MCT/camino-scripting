83eab4df3c3475e004ea811006656a9d4cfb64f9
Adrien Risser <risseraka@gmail.com>
2019-06-13 23:27:11 +0200

    fix(valide-geojson): WIP

--- valide-geojsons/index.js
+++ valide-geojsons/index.js
@@ -2,64 +2,90 @@ const turf = require('@turf/turf')
 const fs = require('fs')
 const path = require('path')
 
+const coordinatesValideGet = coordinates => {
+  // Enlever les [] contenant coord si l'on rajoute des polygones au lieu de rajouter des trous
+  const polygon = turf.polygon([coordinates])
+
+  if (coordinates.length !== 5) return null
+
+  // On crée l'enveloppe convexe des coordonnées (le rectangle le plus petit contenant tout les points)
+  const convex = turf.convex(polygon)
+
+  // L'enveloppe n'ayant pas exactement les mêmes coordonées que le polygone, on calcule l'aire des 2
+  // L'aire d'un papillon est bien plus petite que l'aire d'un polygone fermé.
+  // On néglige les erreurs d'approximation des coordonnées (approx < 1 mm)
+  const ratio = turf.area(convex) / turf.area(polygon)
+
+  return ratio >= 1.01 ? turf.getCoords(convex) : null
+}
+
+const featureValidate = feature =>
+  turf
+    // On construit des trous au lieu de rajouter des polygones.
+    // Note : enlever le '[0]' si l'on modifie la source.
+    // Ne fonctionnera pas si l'on rajoute des polygones et des trous
+    .getCoords(feature)[0]
+    .reduce((arr, coord) => {
+      const coordinatesValide = coordinatesValideGet(coord)
+
+      return coordinatesValide ? [...arr, coordinatesValide] : arr
+    }, [])
+
 const run = () => {
-  let nombrePolygonePapillon = 0
-  const geojsons = require(path.join(process.cwd(), 'sources/geojsons.json'))
-  geojsonsModified = turf.featureCollection(
-    turf.featureReduce(
-      geojsons,
-      (geoArr, feature) => {
-        const coordsEnveloppe = turf
-          // On construit des trous au lieu de rajouter des polygones, enlever le '[0]' si l'on modifie la source. Ne fonctionnera pas si l'on rajoute des polygone et des trous
-          .getCoords(feature)[0]
-          .reduce((arr, coord) => {
-            // Enlever les [] contenant coord si l'on rajoute des polygones au lieu de rajouter des trous
-            const polygon = turf.polygon([coord])
+  const fileName = process.argv[2] || 'sources/geojsons.geojson'
 
-            // Pour avoir toutes les features
-            // if (coord.length !== 5) return [...arr, turf.getCoords(polygon)]
+  const filePath = path.join(process.cwd(), fileName)
 
-            // Pour ne garder que les features erronées
-            if (coord.length !== 5) return arr
+  console.log({ fileName, filePath })
 
-            // On crée l'enveloppe convexe des coordonnées (le rectangle le plus petit contenant tout les points)
-            const convex = turf.convex(polygon)
+  let geojsons
 
-            // L'enveloppe n'ayant pas exactement les mêmes coordonées que le polygone, on calcule l'aire des 2
-            // L'aire d'un papillon est bien plus petite que l'aire d'un polygone fermé. On néglige les erreurs d'approximation des coordonnées (approx < 1 mm)
-            const ratio = turf.area(convex) / turf.area(polygon)
-            if (ratio >= 1.01) {
-              nombrePolygonePapillon += 1
-              return [...arr, turf.getCoords(convex)]
-            }
+  try {
+    geojsons = fs.readFileSync(filePath)
+  } catch (e) {
+    console.error(`Impossible de lire le fichier : ${fileName}`)
+    process.exit(1)
+  }
 
-            // Pour avoir toutes les features
-            // return [...arr, turf.getCoords(polygon)]
+  const features = turf.featureReduce(
+    geojsons,
+    (geoArr, feature) => {
+      const coordsEnveloppe = featureValidate(feature)
+      if (coordsEnveloppe.length === 0) return geoArr
 
-            // Pour ne garder que les features erronées
-            return arr
-          }, [])
+      const geometry = {
+        type: turf.getType(feature),
+        coordinates: coordsEnveloppe
+      }
 
-        if (coordsEnveloppe.length === 0) return geoArr
+      const geom = {
+        ...turf.feature(geometry),
+        properties: feature.properties
+      }
 
-        const geometry = {
-          type: turf.getType(feature),
-          coordinates: coordsEnveloppe
-        }
-        const geom = turf.feature(geometry)
-        geom.properties = feature.properties
-        return [...geoArr, geom]
-      },
-      []
-    )
+      return [...geoArr, geom]
+    },
+    []
   )
-  console.log(nombrePolygonePapillon)
 
-  fs.writeFileSync(
-    path.join(process.cwd(), 'geojsons-modified.json'),
-    JSON.stringify(geojsonsModified)
+  if (!features.length) {
+    console.info('Aucun périmètre invalide détecté')
+    process.exit(0)
+  }
+
+  console.log(features.length)
+
+  const geojsonsModified = turf.featureCollection(features)
+
+  const resultPath = path.join(
+    process.cwd(),
+    `${path.basename(fileName, '.geojson')}-modified.geojson`
   )
-  process.exit()
+  console.log({ resultPath })
+
+  fs.writeFileSync(resultPath, JSON.stringify(geojsonsModified))
+
+  process.exit(0)
 }
 
 run()
