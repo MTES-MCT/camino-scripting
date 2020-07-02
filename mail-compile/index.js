156eefb3d332d1f88eba0313acab3b3064624e20
Adrien Risser <risseraka@gmail.com>
2020-07-02 15:54:52 +0200

    feat(imports-db): permet de préciser un dossier pour la compilation de mails

--- mail-compile/index.js
+++ mail-compile/index.js
@@ -1,38 +1,53 @@
-const fs = require('fs').promises;
-const json2csv = require('json2csv').parse;
+const fs = require("fs").promises;
+const { join } = require("path");
+const json2csv = require("json2csv").parse;
 
-async function format(file) {
+async function format(dir, file) {
   let content;
 
   try {
-    content = (await fs.readFile(`./${file}`)).toString().split('\r\n');
+    content = (await fs.readFile(join(dir, file))).toString().split("\r\n");
   } catch (e) {
+    console.error(e);
     return {};
   }
 
-  let [sujet, mail, date] = content.slice(0, 10).reduce((r, e) => {
-    if (e.match(/Sujet :/)) {
-      r[0] = e;
-    } else if (e.match(/De :/)) {
-      r[1] = e;
-    } else if (e.match(/Date :/)) {
-      r[2] = e;
-    }
+  let [sujet, mail, date] = content.slice(0, 10).reduce(
+    (r, e) => {
+      if (e.match(/Sujet :/)) {
+        r[0] = e;
+      } else if (e.match(/De :/)) {
+        r[1] = e;
+      } else if (e.match(/Date :/)) {
+        r[2] = e;
+      }
 
-    return r;
-  }, ['Sujet :', 'De :', 'Date :']);
+      return r;
+    },
+    ["Sujet :", "De :", "Date :"]
+  );
 
   let text;
 
   try {
-    sujet = sujet.split(':')[1].trim()
-    mail = mail.split(' ').pop().slice(1, -1);
-    date = date.split(':').slice(1).join(':').trim()
+    sujet = sujet.split(":")[1].trim();
+    mail = mail
+      .split(" ")
+      .pop()
+      .slice(1, -1);
+    date = date
+      .split(":")
+      .slice(1)
+      .join(":")
+      .trim();
 
-    text = content.slice(5).join('\n').trim();
+    text = content
+      .slice(5)
+      .join("\n")
+      .trim();
   } catch (e) {
-    console.error(e);
     console.log({ file, content });
+    console.error(e);
     return {};
   }
 
@@ -40,10 +55,18 @@ async function format(file) {
 }
 
 async function main() {
-  let files = await fs.readdir('.');
-  files = files.filter(n => n !== 'output.txt' && n.match(/\.txt$/));
+  const dir = process.argv[2];
+  if (!dir) {
+    console.error("usage: node index.js dossier");
+    process.exit(1);
+  }
 
-  const lines = await Promise.all(files.map(format).slice(0));
+  let files = await fs.readdir(dir);
+  files = files.filter(n => n !== "output.txt" && n.match(/\.txt$/));
+
+  const lines = await Promise.all(
+    files.map(file => format(dir, file)).slice(0)
+  );
 
   const fields = Object.keys(lines[0]);
 
